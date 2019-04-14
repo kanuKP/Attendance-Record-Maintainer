c0e4c8384d8f05b7d776891730eeda947d3c3890
Kanu Priya <kanu28kanu@gmail.com>
2019-04-14 20:40:14 +0530

    Project Files

---
+++ src/Attendanceproj.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author Kanu
+ */
+public class Attendanceproj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Entryportal f1 = new Entryportal();
+        f1.setVisible(true);
+    }
+}
